2dc0489003ddbe3ecd5038d637d887a3df8f1f9b
Cristian Greco <cristian.greco@itv.com>
2015-10-11 19:47:19 +0100

    FrogJmp

--- src/FrogJmp.java
+++ src/FrogJmp.java
@@ -1,2 +1,23 @@
 public class FrogJmp {
+
+    public int solution(int X, int Y, int D) {
+        if (X >= Y) {
+            return 0;
+        }
+
+        int steps = 0;
+
+        do {
+            steps++;
+            X += D;
+        } while (X < Y);
+
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        assert new FrogJmp().solution(10, 85, 30) == 3;
+        assert new FrogJmp().solution(1, 1, 3) == 0;
+    }
+
 }
