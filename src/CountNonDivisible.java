39243b6c6a69977b6eaf2e824c303db669364314
Cristian Greco <cristian.greco@itv.com>
2015-10-20 18:33:15 +0100

    CountNonDivisible complete

--- src/CountNonDivisible.java
+++ src/CountNonDivisible.java
@@ -1,26 +1,23 @@
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.Arrays;
 
 public class CountNonDivisible {
 
     int[] solution(int[] A) {
-        Map<Integer, Integer> results = new TreeMap<>();
+        int[] results = new int[A.length];
         for (int i = 0; i < A.length; i++) {
-            results.put(A[i], 0);
+            results[i] = 0;
             for (int j = 0; j < A.length; j++) {
                 if (i == j) continue;
                 if (A[i] % A[j] != 0) {
-                    int currentCount = results.get(A[i]);
-                    results.put(A[i], currentCount + 1);
+                    results[i]++;
                 }
             }
         }
-        System.out.println(results);
-        return null;
+        return results;
     }
 
     public static void main(String[] args) {
-        assert new CountNonDivisible().solution(new int[]{3, 1, 2, 3, 6}) == new int[]{2, 4, 3, 2, 0};
+        assert Arrays.equals(new CountNonDivisible().solution(new int[]{3, 1, 2, 3, 6}), new int[]{2, 4, 3, 2, 0});
     }
 
 }
